0c512286b0810bf73951564b4831b168714af719
jashonmichael <jashon.michael@student.moringaschool.com>
2024-05-27 07:04:12 +0300

    Complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        self.sorted_word = sorted(word)
+    
+    def match(self, word_list):
+        return [word for word in word_list if sorted(word) == self.sorted_word]
+
+listen = Anagram("listen")
+print(listen.match(['enlists', 'google', 'inlets', 'banana']))
